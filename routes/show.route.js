651a38a34000926a2f5df82c49457b822ddf478c
danny paiz <daniel.martinez.paiz@verizon.com>
2024-09-27 15:38:04 -0400

    show.route, user.route, successfully connected to able to perform crud operations. Note: routes needed to require object of our models as it was exported with { }

---
+++ routes/show.route.js
@@ -0,0 +1,33 @@
+const express = require("express")
+// const User = require("../models/index")
+const { Show } = require("../models/index")
+
+const showRouter = express.Router()
+
+// CREATE ONE SHOW
+showRouter.post("/create", async (req, res) => {
+    const createShow = await Show.create(req.body)
+    res.json(createShow)
+})
+
+// GET ALL SHOWS
+showRouter.get("/", async (req, res) => {
+    let allShows = await Show.findAll()
+    res.json(allShows)
+})
+
+showRouter.get("/:id", async (req, res) => {
+    let oneShow = await Show.findByPk(req.params.id)
+    res.json(showRouter)
+})
+
+showRouter.delete("/remove/:id", async (req, res) => {
+    const deleteShow = await Show.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.json(deleteShow)
+})
+
+module.exports = showRouter
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-
+const showRouter = require("../routes/show.route")
+const userRouter = require("../routes/user.route")
 // ROUTER NEEDED - update me
 // const router = require("../routes/CHANGE_ME.route")
 
@@ -10,5 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // UPDATE ME
 // app.use("/", router)
+app.use("/shows", showRouter)
+app.use("/users", userRouter)
 
 module.exports = app
